3d42ff573fa6674eb255e945d860b39c6fd1a6bb
Artem Tansan <tansan@omnicomm.ru>
2017-09-04 10:41:14 +0300

    Проверка типа переменных и значений

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    String somebody = "world";
     System.out.println("Hello, world!");
+
+    double l = 8.0;
+    double s = l * l;
+    System.out.println("Плошадь квадрата со стороной " + l + " = " +s);
   }
 
 }
